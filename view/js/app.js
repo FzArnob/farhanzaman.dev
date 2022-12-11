cee065ba40e1f41bf14931c75ac0b8b55faf7257
User's Full Name <farhanzamanarnob@gmail.com>
2022-12-11 20:34:08 +0600

    1

---
+++ view/js/app.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelector("#theme-toogle-button").addEventListener('click', function (e) {
+
+    });
+    document.querySelector("#nav-menu.bar-items").addEventListener('mouseover', function (ele) {
+        
+    });
+});
