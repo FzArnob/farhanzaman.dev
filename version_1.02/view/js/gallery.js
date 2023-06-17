5d812c9f503cd2d7067013ab48545dbdacefa9e7
User's Full Name <farhanzamanarnob@gmail.com>
2023-06-17 12:18:11 +0600

    gallery page added

--- version_1.02/view/js/gallery.js
+++ version_1.02/view/js/gallery.js
@@ -94,61 +94,6 @@ fetchProfileData()
         console.error(error); // Handle any errors here
     });
 
-document.getElementById("direct-message").addEventListener("submit", function (event) {
-    event.preventDefault();
-    var name = document.getElementById("message-name");
-    var email = document.getElementById("message-email");
-    var subject = document.getElementById("message-subject");
-    var message = document.getElementById("message");
-    var header = new Headers();
-    header.append("Content-Type", "application/x-www-form-urlencoded");
-
-    var urlencoded = new URLSearchParams();
-    urlencoded.append("profile_id", "farhan");
-    urlencoded.append("name", name.value);
-    urlencoded.append("email", email.value);
-    urlencoded.append("subject", subject.value);
-    urlencoded.append("message", message.value);
-
-    var requestOptions = {
-        method: 'POST',
-        headers: header,
-        body: urlencoded,
-        redirect: 'follow'
-    };
-
-    fetch(host + "/send-direct-message.php", requestOptions)
-        .then(function (response) {
-            if (response.ok) {
-                return response.json()
-                    .then(function (result) {
-                        // Process the response data here
-                        console.log(result);
-                        document.getElementById("sendmessage").style.display = 'block';
-                        name.value = "";
-                        email.value = "";
-                        subject.value = "";
-                        message.value = "";
-                        setTimeout(function () {
-                            document.getElementById("sendmessage").style.display = 'none';
-                        }, 3000);
-                    });
-            } else {
-                return response.json()
-                    .then(function (result) {
-                        throw new Error(result.message);
-                    });
-            }
-        })
-        .catch(function (error) {
-            console.log("Request error:", error);
-            document.getElementById("errormessage").innerHTML = error.message;
-            document.getElementById("errormessage").style.display = 'block';
-            setTimeout(function () {
-                document.getElementById("errormessage").style.display = 'none';
-            }, 4000);
-        });
-});
 
 
 
@@ -185,19 +130,8 @@ function createGallery(imageUrls) {
             break;
     }
     var imageSide = (window.innerWidth / numColumn) - 70;
-
-    const containerHeight = imageSide + (imageSide / 2) + 30;
-    const containerShadowHeight = imageSide / 2;
-
-    container.style.height = containerHeight + 'px';
-
-    const containerShadow = document.createElement('div');
-    containerShadow.style.height = containerShadowHeight + 'px';
-    containerShadow.style.top = containerHeight - containerShadowHeight - 1 + 'px';
-    containerShadow.classList.add('bottom-gradient-bg');
-    containerShadow.classList.add('gallery-container-shadow');
     console.log(numColumn);
-    for (let i = 0; i < numColumn * 2; i++) {
+    for (let i = 0; i < imageUrls.length; i++) {
         var url = imageUrls[i];
         const image = document.createElement('img');
         image.src = url;
@@ -240,58 +174,8 @@ function createGallery(imageUrls) {
         container.appendChild(imageContainer);
     }
 
-    container.appendChild(containerShadow);
     document.getElementById('gallery').appendChild(container);
 }
-
-function createSkillCloud(skillTags) {
-    var radius;
-    if (window.innerWidth <= 800) {
-        radius = window.innerWidth / 2;
-    } else {
-        radius = window.innerWidth / 4;
-        if (radius > 400) radius = 400;
-    }
-    var tagCloud = TagCloud(".skillTags", skillTags, {
-        // radius in px
-        radius: radius,
-
-        // animation speed
-        // slow, normal, fast
-        maxSpeed: "fast",
-        initSpeed: "normal",
-
-        // 0 = top
-        // 90 = left
-        // 135 = right-bottom
-        direction: 135,
-
-        // interact with cursor move on mouse out
-        keep: true,
-    });
-    var textSize;
-    if (window.innerWidth <= 1400) {
-        textSize = Math.floor(27 * radius / 300);
-    } else {
-        textSize = Math.floor(22 * radius / 300);
-    }
-    // console.log(radius,"--" ,Math.floor(20*radius/300));
-    document.getElementById("skill-canvas").firstChild.style.fontSize = textSize + "px";
-    // console.log(document.getElementById("skill-canvas").firstChild.style.fontSize);
-
-    //To change the color of text randomly
-    // var colors = ["#34A853", "#FBBC05", "#4285F4", "#7FBC00", "FFBA01", "01A6F0"];
-    // var random_color = colors[Math.floor(Math.random() * colors.length)];
-    // document.querySelector(".content").style.color = random_color;
-}
-
-
-
-
-
-
-
-
 
 
 
@@ -306,13 +190,7 @@ function isInViewport(element) {
     );
 }
 
-function smoothScroll(target) {
-    // Scroll to the target element smoothly
-    document.querySelector(target).scrollIntoView({
-      behavior: 'smooth'
-    });
-  }
-  
+
 function wobbleAnimation() {
     var wobbleElements = document.querySelectorAll(".wobble:not([data-animate-once])");
     wobbleElements.forEach(function (el) {
@@ -368,7 +246,6 @@ function wobbleAnimation() {
 
 document.addEventListener("DOMContentLoaded", function () {
 
-    document.getElementById("content-gap").style.marginTop = window.innerHeight - 70 + "px";
     // preload
     if (getCookie("theme") === "") {
         var themePopUp = document.getElementById("theme-popup");
@@ -442,80 +319,6 @@ document.addEventListener("DOMContentLoaded", function () {
 
         });
 
-    //   intro animation
-    var mousePos = {};
-
-    function getRandomInt(min, max) {
-        return Math.round(Math.random() * (max - min + 1)) + min;
-    }
-    function hexToRgb(hex) {
-        var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-        return result
-            ? {
-                r: parseInt(result[1], 16),
-                g: parseInt(result[2], 16),
-                b: parseInt(result[3], 16),
-            }
-            : {
-                r: null,
-                g: null,
-                b: null,
-            }
-    }
-    document.getElementById("wrap").addEventListener("mousemove", function (e) {
-        mousePos.x = e.pageX;
-        mousePos.y = e.pageY;
-        var rangex = 15;
-        var rangey = 15;
-        var sizeInt = getRandomInt(20, 30);
-        if (mousePos.x > 0 && mousePos.y > 0) {
-
-            var colorList = [
-                "#80ffec",
-                "#4dffe5",
-                "#00e6c4",
-
-                "#fe819e",
-                "#fd4e78",
-                "#fd1c51",
-
-                "#c4c4c4",
-            ];
-            var colorCode = colorList[getRandomInt(0, colorList.length - 1)];
-            var colorRGB = hexToRgb(colorCode);
-            var color =
-                "background: transparent; border: 2px solid " +
-                colorCode +
-                "; box-shadow: 0 0 14px rgba(" +
-                colorRGB.r +
-                ", " +
-                colorRGB.g +
-                ", " +
-                colorRGB.b +
-                ", 0.6);";
-
-            var size = "height: " + sizeInt + "px; width: " + sizeInt + "px;";
-
-            var left =
-                "left: " +
-                getRandomInt(mousePos.x - rangex - sizeInt, mousePos.x + rangex) +
-                "px;";
-
-            var top =
-                "top: " +
-                getRandomInt(mousePos.y - rangey - sizeInt, mousePos.y + rangey) +
-                "px;";
-
-            var style = left + top + color + size;
-            const ball = document.createElement("div");
-            ball.classList.add("ball");
-            ball.setAttribute("style", style);
-            ball.addEventListener("animationend", function (e) {
-                this.parentNode.removeChild(this);
-            });
-            document.getElementById("wrap").appendChild(ball);
-        }
-    });
     //animation text
     function addListenerMulti(element, eventNames, listener) {
         var events = eventNames.split(' ');
@@ -567,15 +370,6 @@ document.addEventListener("DOMContentLoaded", function () {
 
     window.addEventListener("scroll", wobbleAnimation);
 
-    // intro image animation
-    var introImageEle = document.getElementById("intro-image");
-    introImageEle.addEventListener("mousemove", function (e) {
-        introImageEle.style.opacity = 0.8;
-    });
-
-    introImageEle.addEventListener("mouseleave", function (e) {
-        introImageEle.style.opacity = 0.4;
-    });
     window.addEventListener('scroll', function () {
         var button = document.getElementById('back-to-top-btn');
         if (window.pageYOffset > 300) {
@@ -592,33 +386,6 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 
-//Test 
-const skillTags = [
-    "HTML",
-    "CSS",
-    "JavaScript",
-    "TypeScript",
-    "Python",
-    "Java",
-    "C++",
-    "PHP",
-    "SpringBoot",
-    "ReactJS",
-    "MongoDB",
-    "NodeJS",
-    "MySQL",
-    "NoSQL",
-    "GraphQL",
-    "Selenium",
-    "OOD",
-    "Redux",
-    "jQuery",
-    "Photoshop",
-    "Illustrator",
-    "Excel",
-    "PowerPoint"
-];
-createSkillCloud(skillTags);
 
 const imageUrls = [
     "https://drive.google.com/uc?export=view&id=1uTNMfmAJ37-eT0bTfvlEbBOkV_o9xAN2",
@@ -697,81 +464,6 @@ const photos = [
 ];
 createGallery(imageUrls);
 
-
-
-function generateSkillBars(skills) {
-    // Create the skill bars container div
-    const skillBarsDiv = document.getElementById("skill-bars");
-    skillBarsDiv.classList.add('skill-bars');
-
-    var count;
-    if (window.innerWidth <= 800) count = Math.floor((window.innerWidth) / 72);
-    else count = Math.floor((window.innerWidth / 4) / 72);
-
-    // Create the individual skill bars
-    for (let i = 0; i < count; i++) {
-        const skill = skills[i];
-        // Create the skill bar div
-        const skillBarDiv = document.createElement('div');
-        skillBarDiv.classList.add('bar');
-
-        // Create the info div
-        const infoDiv = document.createElement('div');
-        infoDiv.classList.add('info');
-
-        // Create the span element inside the info div
-        const spanElement = document.createElement('span');
-        spanElement.textContent = skill.name;
-
-        // Append the span element to the info div
-        infoDiv.appendChild(spanElement);
-
-        // Create the progress line div
-        const progressLineDiv = document.createElement('div');
-        progressLineDiv.classList.add('progress-line');
-
-        // Create the span element inside the progress line div
-        const spanElement2 = document.createElement('span');
-        spanElement2.style.width = skill.width * 20 + '%';
-        spanElement2.setAttribute('data-value', skill.width + "/5")
-
-        // Append the span element to the progress line div
-        progressLineDiv.appendChild(spanElement2);
-
-        // Append the info div and progress line div to the skill bar div
-        skillBarDiv.appendChild(infoDiv);
-        skillBarDiv.appendChild(progressLineDiv);
-
-        // Append the skill bar div to the skill bars container div
-        skillBarsDiv.appendChild(skillBarDiv);
-    }
-}
-function skillBarAnimation() {
-    const element = document.getElementById('skill-bars');
-    if (!element.classList.contains('in-viewport')) {
-        if (isInViewport(element)) {
-            element.classList.add('in-viewport');
-            // Skill data
-            const skills = [
-                { name: 'Problem Solving', width: 4.6, className: 'ps' },
-                { name: 'Critical thinking', width: 4, className: 'ct' },
-                { name: 'Teamwork', width: 3.3, className: 'tw' },
-                { name: 'Leadership', width: 4.1, className: 'bc' },
-                { name: 'Interpersonal skills', width: 3.2, className: 'is' },
-                { name: 'Adaptability', width: 2.55, className: 'a' },
-                { name: 'Business Communication', width: 4, className: 'l' },
-                { name: 'Presentation skills', width: 4, className: 'psk' },
-                { name: 'Creativity', width: 3, className: 'c' },
-                { name: 'Self-motivated', width: 3, className: 'sm' },
-                { name: 'Organizational skills', width: 2, className: 'os' },
-                { name: 'Time management', width: 3, className: 'tm' }
-            ];
-            generateSkillBars(skills);
-        }
-    }
-
-}
-window.addEventListener("scroll", skillBarAnimation);
 
 function createPhotoViewer(targetElement, photos) {
     const photoContainer = document.getElementById(targetElement);
