825346f64279504431171105311d8f389c421fc7
Md. Farhan Zaman <farhanzamanarnob@gmail.com>
2024-02-05 05:57:44 +0600

    Update index.php

--- version_1.05/index.php
+++ version_1.05/index.php
@@ -4,23 +4,29 @@ $requestUri = $_SERVER['REQUEST_URI'];
 // Remove leading and trailing slashes and any potential query strings
 $path = trim(parse_url($requestUri, PHP_URL_PATH), '/');
 
+// Split the path into parts
 $parts = explode("/", $path);
 
+// Get the route from the first part
 $route = isset($parts[0]) ? $parts[0] : 'none';
+
+// If there are query parameters, remove them from the route
+if (strpos($route, '?') !== false) {
+    list($route, $queryString) = explode('?', $route, 2);
+}
 
 // Define the valid routes and their corresponding HTML files
 $routes = array(
     ''         => 'home.html',    // Home page
     'about'    => 'about.html',
     'works'    => 'works.html',
-    'work' => 'work.html',
+    'work'     => 'work.html',
     'hobbies'  => 'hobbies.html',
     'expertise' => 'expertise.html',
-    '403' => '403.html',
-    '404' => '404.html',
-    '500' => '500.html'
+    '403'      => '403.html',
+    '404'      => '404.html',
+    '500'      => '500.html'
 );
-
 
 // Check if the requested route exists in the defined routes
 if (isset($routes[$route])) {
@@ -29,7 +35,7 @@ if (isset($routes[$route])) {
     if (file_exists($file)) {
         // Set the appropriate content type
         header('Content-Type: text/html');
-        // // Read and output the HTML file
+        // Read and output the HTML file
         readfile($file);
         exit;
     }
@@ -37,3 +43,4 @@ if (isset($routes[$route])) {
     header('Location: /404');
     exit;
 }
+?>
