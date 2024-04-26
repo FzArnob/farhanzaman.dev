422bbd3bf7eebb5a39281cabb8bfa7cb3104e557
Md. Farhan Zaman <farhanzamanarnob@gmail.com>
2024-04-26 16:12:51 +0600

    user agent parser updated

--- version_1.06/backend/api/test3.php
+++ version_1.06/backend/api/test3.php
@@ -1,37 +1,84 @@
 <?php
 function parseUserAgent($userAgent)
 {
-    $browserNames = array('OPR/', 'MiuiBrowser/', 'HuaweiBrowser/', 'VivoBrowser/', 'HeyTapBrowser/', 'Silk/', 'Edge/', 'Presto/', 'EdgA/', 'SamsungBrowser/', 'UCBrowser/','Brave/', 'Vivaldi/', 'YaBrowser/', 'Discord/', 'Twitter', 'LinkedIn/', 'Snapchat/', 'Pinterest/', 'WhatsApp/', 'Instagram ', 'FBAV/', 'FBAN/', 'FB_IAB/', 'FB4A/','FBIOS/', 'Chrome/', 'Firefox/', 'Safari/');
-    // Opera Browser
-    $operatingSystems = array('Windows', 'Macintosh', 'Windows Phone', 'BBE', 'Android', 'iPhone', 'iPod', 'iPad', 'CrOS', 'iOS', 'CentOS', 'RHEL', 'FreeBSD', 'PlayStation', 'Ubuntu', 'Fedora','Linux');
-    // Microsoft Windows
-    // Mac OS
-    // Red Hat Enterprise Linux
-    // Chrome OS
+    $browserNames = array('OPR/', 'MiuiBrowser/', 'HuaweiBrowser/', 'VivoBrowser/', 'HeyTapBrowser/', 'Silk/', 'Edge/', 'Presto/', 'EdgA/', 'SamsungBrowser/', 'UCBrowser/', 'Brave/', 'Vivaldi/', 'YaBrowser/', 'Discord/', 'Twitter-', 'LinkedIn/', 'Snapchat/', 'Pinterest/', 'WhatsApp/', 'Instagram ', 'FBAV/', 'FBAN/', 'FB_IAB/', 'FB4A/', 'FBIOS/', 'Chrome/', 'Firefox/', 'Safari/');
+    $operatingSystems = array('Windows', 'Macintosh', 'Windows Phone', 'BBE', 'Android', 'iPhone', 'iPod', 'iPad', 'CrOS', 'iOS', 'CentOS', 'RHEL', 'FreeBSD', 'PlayStation', 'Ubuntu', 'Fedora', 'Linux');
+    $browserNamesMap = array(
+        'OPR/' => 'Opera Browser',
+        'MiuiBrowser/' => 'Mi Browser',
+        'HuaweiBrowser/' => 'Huawei Browser',
+        'VivoBrowser/' => 'Vivo Browser',
+        'HeyTapBrowser/' => 'HeyTap Browser',
+        'Silk/' => 'Amazon Silk',
+        'Edge/' => 'Microsoft Edge',
+        'Presto/' => 'Opera (Old)',
+        'EdgA/' => 'Microsoft Edge (Android)',
+        'SamsungBrowser/' => 'Samsung Browser',
+        'UCBrowser/' => 'UC Browser',
+        'Brave/' => 'Brave Browser',
+        'Vivaldi/' => 'Vivaldi Browser',
+        'YaBrowser/' => 'Yandex Browser',
+        'Discord/' => 'Discord',
+        'Twitter-' => 'Twitter',
+        'LinkedIn/' => 'LinkedIn',
+        'Snapchat/' => 'Snapchat',
+        'Pinterest/' => 'Pinterest',
+        'WhatsApp/' => 'WhatsApp',
+        'Instagram ' => 'Instagram',
+        'FBAV/' => 'Facebook (Android)',
+        'FBAN/' => 'Facebook (iOS)',
+        'FB_IAB/' => 'Facebook (In-App Browser)',
+        'FB4A/' => 'Facebook (Android)',
+        'FBIOS/' => 'Facebook (iOS)',
+        'Chrome/' => 'Google Chrome',
+        'Firefox/' => 'Mozilla Firefox',
+        'Safari/' => 'Safari'
+    );
+
+    $operatingSystemsMap = array(
+        'Windows' => 'Microsoft Windows',
+        'Macintosh' => 'Mac OS',
+        'Windows Phone' => 'Windows Phone',
+        'BBE' => 'BlackBerry OS',
+        'Android' => 'Android',
+        'iPhone' => 'iOS (iPhone)',
+        'iPod' => 'iOS (iPod)',
+        'iPad' => 'iOS (iPad)',
+        'CrOS' => 'Chrome OS',
+        'iOS' => 'iOS',
+        'CentOS' => 'CentOS',
+        'RHEL' => 'Red Hat Enterprise Linux',
+        'FreeBSD' => 'FreeBSD',
+        'PlayStation' => 'PlayStation OS',
+        'Ubuntu' => 'Ubuntu',
+        'Fedora' => 'Fedora',
+        'Linux' => 'Linux'
+    );
+
     $deviceTypes = array(
-        'Mobile' => array('iPhone', 'Android', 'Windows Phone', 'BBE', 'Mobile'),
-        'Desktop' => array('Windows', 'Macintosh', 'Linux', 'X11', 'CrOS'),
-        'Tablet' => array('iPad', 'Touch', 'SM-', ),
-        'SmartTV' => array('SMART-TV'),
         'Console' => array('PlayStation', 'Xbox', 'Nintendo'),
         'Wearable' => array('SmartWatch', 'Wearable'),
-        'Bot' => array('bot', 'spider', 'crawl', 'googlebot')
+        'Bot' => array('bot', 'spider', 'crawl', 'googlebot'),
+        'SmartTV' => array('SMART-TV', 'CrKey/', 'Silk/', 'Dalvik/', 'SmartTV', 'Smart TV', 'BRAVIA'),
+        'Tablet' => array('iPad', 'Touch', 'SM-', 'Surface', 'Lenovo TB', 'MatePad', 'KFMEWI', 'KFSUWI', 'Pixel Slate', 'Asus_Z', 'Sony SG', 'Mi Pad', 'Venue', 'Acer B', 'HP Elite', 'LG-V', 'Alcatel_', 'Toughbook', 'Fujitsu Q', 'Toshiba AT', 'Archos Oxygen', 'Wacom Cintiq', 'Xoom', 'PlayBook', 'ASUS Transformer', 'HTC Flyer', 'Grand X', 'ViewPad', 'Multipad', 'Nextbook', 'Onda V', 'Ramos', 'Pipo', 'Tablet'),
+        'Mobile' => array('iPhone', 'Android', 'Windows Phone', 'BBE', 'Mobile'),
+        'Desktop' => array('Windows', 'Macintosh', 'Linux', 'X11', 'CrOS', 'Ubuntu', 'Fedora')
     );
 
     $info = array(
-        'browser_name' => '',
-        'browser_version' => '',
-        'operating_system' => '',
-        'device_type' => '',
-        'device_details' => '',
-        'rendering_engine' => '',
-        'mobile_specific_info' => ''
+        'browser_name' => 'Other',
+        'browser_version' => 'N/A',
+        'operating_system' => 'Other',
+        'device_type' => 'Other',
+        'device_details' => 'N/A',
+        'rendering_engine' => 'N/A',
+        'mobile_specific_info' => 'false'
     );
 
     // Extract browser name and version
     foreach ($browserNames as $browser) {
         if (strpos($userAgent, $browser) !== false) {
-            $info['browser_name'] = $browser;
+            $info['browser_name'] = $browserNamesMap[$browser];
             $info['browser_version'] = getBrowserVersion($userAgent, $browser);
             break;
         }
@@ -40,7 +87,7 @@ function parseUserAgent($userAgent)
     // Extract operating system
     foreach ($operatingSystems as $os) {
         if (strpos($userAgent, $os) !== false) {
-            $info['operating_system'] = $os;
+            $info['operating_system'] = $operatingSystemsMap[$os];
             break;
         }
     }
@@ -63,8 +110,8 @@ function parseUserAgent($userAgent)
 
     // Extract mobile-specific information (if available)
     if ($info['device_type'] === 'Mobile') {
-        $info['mobile_specific_info'] = getMobileSpecificInfo($userAgent);
-    }
+        $info['mobile_specific_info'] = 'true';
+    } else $info['mobile_specific_info'] = 'false';
 
     return $info;
 }
@@ -74,9 +121,7 @@ function parseUserAgent($userAgent)
 function getBrowserVersion($userAgent, $browser)
 {
     $startPos = strpos($userAgent, $browser) + strlen($browser) + 1;
-    $endPos = strpos($userAgent.' ', ' ', $startPos);
-    echo $startPos;
-    echo $endPos;
+    $endPos = strpos($userAgent . ' ', ' ', $startPos);
     $version = substr($userAgent, $startPos, $endPos - $startPos);
     return $version;
 }
@@ -99,17 +144,5 @@ function getRenderingEngine($userAgent)
     return $engine;
 }
 
-// Helper function to extract mobile-specific information
-function getMobileSpecificInfo($userAgent)
-{
-    $info = '';
-    if (strpos($userAgent, 'Mobile') !== false) {
-        // Extract additional mobile-specific info, such as screen resolution or pixel density
-        // Modify this section as per your requirements
-        $info = 'Some mobile-specific information';
-    }
-    return $info;
-}
-$userAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.128 Safari/537.36 OPR/75.0.3969.285 ";
+$userAgent = "Mozilla/5.0 ";
 print_r(parseUserAgent($userAgent));
-?>
--- version_1.06/backend/models/ProfileModel.php
+++ version_1.06/backend/models/ProfileModel.php
@@ -9,32 +9,84 @@ class ProfileModel
 
     function parseUserAgent($userAgent)
     {
-        $browserNames = array('Chrome', 'Firefox', 'Safari', 'Internet Explorer');
-        $operatingSystems = array('Windows', 'Macintosh', 'Android', 'iPhone', 'iPod', 'iPad', 'CrOS', 'iOS', 'Ubuntu', 'Fedora','Linux');
+        $browserNames = array('OPR/', 'MiuiBrowser/', 'HuaweiBrowser/', 'VivoBrowser/', 'HeyTapBrowser/', 'Silk/', 'Edge/', 'Presto/', 'EdgA/', 'SamsungBrowser/', 'UCBrowser/', 'Brave/', 'Vivaldi/', 'YaBrowser/', 'Discord/', 'Twitter-', 'LinkedIn/', 'Snapchat/', 'Pinterest/', 'WhatsApp/', 'Instagram ', 'FBAV/', 'FBAN/', 'FB_IAB/', 'FB4A/', 'FBIOS/', 'Chrome/', 'Firefox/', 'Safari/');
+        $operatingSystems = array('Windows', 'Macintosh', 'Windows Phone', 'BBE', 'Android', 'iPhone', 'iPod', 'iPad', 'CrOS', 'iOS', 'CentOS', 'RHEL', 'FreeBSD', 'PlayStation', 'Ubuntu', 'Fedora', 'Linux');
+        $browserNamesMap = array(
+            'OPR/' => 'Opera Browser',
+            'MiuiBrowser/' => 'Mi Browser',
+            'HuaweiBrowser/' => 'Huawei Browser',
+            'VivoBrowser/' => 'Vivo Browser',
+            'HeyTapBrowser/' => 'HeyTap Browser',
+            'Silk/' => 'Amazon Silk',
+            'Edge/' => 'Microsoft Edge',
+            'Presto/' => 'Opera (Old)',
+            'EdgA/' => 'Microsoft Edge (Android)',
+            'SamsungBrowser/' => 'Samsung Browser',
+            'UCBrowser/' => 'UC Browser',
+            'Brave/' => 'Brave Browser',
+            'Vivaldi/' => 'Vivaldi Browser',
+            'YaBrowser/' => 'Yandex Browser',
+            'Discord/' => 'Discord',
+            'Twitter-' => 'Twitter',
+            'LinkedIn/' => 'LinkedIn',
+            'Snapchat/' => 'Snapchat',
+            'Pinterest/' => 'Pinterest',
+            'WhatsApp/' => 'WhatsApp',
+            'Instagram ' => 'Instagram',
+            'FBAV/' => 'Facebook (Android)',
+            'FBAN/' => 'Facebook (iOS)',
+            'FB_IAB/' => 'Facebook (In-App Browser)',
+            'FB4A/' => 'Facebook (Android)',
+            'FBIOS/' => 'Facebook (iOS)',
+            'Chrome/' => 'Google Chrome',
+            'Firefox/' => 'Mozilla Firefox',
+            'Safari/' => 'Safari'
+        );
+
+        $operatingSystemsMap = array(
+            'Windows' => 'Microsoft Windows',
+            'Macintosh' => 'Mac OS',
+            'Windows Phone' => 'Windows Phone',
+            'BBE' => 'BlackBerry OS',
+            'Android' => 'Android',
+            'iPhone' => 'iOS (iPhone)',
+            'iPod' => 'iOS (iPod)',
+            'iPad' => 'iOS (iPad)',
+            'CrOS' => 'Chrome OS',
+            'iOS' => 'iOS',
+            'CentOS' => 'CentOS',
+            'RHEL' => 'Red Hat Enterprise Linux',
+            'FreeBSD' => 'FreeBSD',
+            'PlayStation' => 'PlayStation OS',
+            'Ubuntu' => 'Ubuntu',
+            'Fedora' => 'Fedora',
+            'Linux' => 'Linux'
+        );
+
         $deviceTypes = array(
-            'Desktop' => array('Windows', 'Macintosh', 'Linux', 'X11', 'CrOS'),
-            'Mobile' => array('iPhone', 'Android', 'Windows Phone', 'BlackBerry', 'Mobile'),
-            'Tablet' => array('iPad', 'Android', 'Tablet'),
-            'SmartTV' => array('SMART-TV'),
             'Console' => array('PlayStation', 'Xbox', 'Nintendo'),
             'Wearable' => array('SmartWatch', 'Wearable'),
-            'Bot' => array('bot', 'spider', 'crawl', 'googlebot')
+            'Bot' => array('bot', 'spider', 'crawl', 'googlebot'),
+            'SmartTV' => array('SMART-TV', 'CrKey/', 'Silk/', 'Dalvik/', 'SmartTV', 'Smart TV', 'BRAVIA'),
+            'Tablet' => array('iPad', 'Touch', 'SM-', 'Surface', 'Lenovo TB', 'MatePad', 'KFMEWI', 'KFSUWI', 'Pixel Slate', 'Asus_Z', 'Sony SG', 'Mi Pad', 'Venue', 'Acer B', 'HP Elite', 'LG-V', 'Alcatel_', 'Toughbook', 'Fujitsu Q', 'Toshiba AT', 'Archos Oxygen', 'Wacom Cintiq', 'Xoom', 'PlayBook', 'ASUS Transformer', 'HTC Flyer', 'Grand X', 'ViewPad', 'Multipad', 'Nextbook', 'Onda V', 'Ramos', 'Pipo', 'Tablet'),
+            'Mobile' => array('iPhone', 'Android', 'Windows Phone', 'BBE', 'Mobile'),
+            'Desktop' => array('Windows', 'Macintosh', 'Linux', 'X11', 'CrOS', 'Ubuntu', 'Fedora')
         );
 
         $info = array(
-            'browser_name' => '',
-            'browser_version' => '',
-            'operating_system' => '',
-            'device_type' => '',
-            'device_details' => '',
-            'rendering_engine' => '',
-            'mobile_specific_info' => ''
+            'browser_name' => 'Other',
+            'browser_version' => 'N/A',
+            'operating_system' => 'Other',
+            'device_type' => 'Other',
+            'device_details' => 'N/A',
+            'rendering_engine' => 'N/A',
+            'mobile_specific_info' => 'false'
         );
 
         // Extract browser name and version
         foreach ($browserNames as $browser) {
             if (strpos($userAgent, $browser) !== false) {
-                $info['browser_name'] = $browser;
+                $info['browser_name'] = $browserNamesMap[$browser];
                 $info['browser_version'] = $this->getBrowserVersion($userAgent, $browser);
                 break;
             }
@@ -43,7 +95,7 @@ class ProfileModel
         // Extract operating system
         foreach ($operatingSystems as $os) {
             if (strpos($userAgent, $os) !== false) {
-                $info['operating_system'] = $os;
+                $info['operating_system'] = $operatingSystemsMap[$os];
                 break;
             }
         }
@@ -66,8 +118,8 @@ class ProfileModel
 
         // Extract mobile-specific information (if available)
         if ($info['device_type'] === 'Mobile') {
-            $info['mobile_specific_info'] = $this->getMobileSpecificInfo($userAgent);
-        }
+            $info['mobile_specific_info'] = 'true';
+        } else $info['mobile_specific_info'] = 'false';
 
         return $info;
     }
@@ -77,7 +129,7 @@ class ProfileModel
     function getBrowserVersion($userAgent, $browser)
     {
         $startPos = strpos($userAgent, $browser) + strlen($browser) + 1;
-        $endPos = strpos($userAgent, ' ', $startPos);
+        $endPos = strpos($userAgent . ' ', ' ', $startPos);
         $version = substr($userAgent, $startPos, $endPos - $startPos);
         return $version;
     }
@@ -98,18 +150,6 @@ class ProfileModel
         $endPos = strpos($userAgent, ' ', $startPos);
         $engine = substr($userAgent, $startPos, $endPos - $startPos);
         return $engine;
-    }
-
-    // Helper function to extract mobile-specific information
-    function getMobileSpecificInfo($userAgent)
-    {
-        $info = 'Not_Mobile';
-        if (strpos($userAgent, 'Mobile') !== false) {
-            // Extract additional mobile-specific info, such as screen resolution or pixel density
-            // Modify this section as per your requirements
-            $info = 'Mobile';
-        }
-        return $info;
     }
     public function saveVisitorData($profile_id)
     {
