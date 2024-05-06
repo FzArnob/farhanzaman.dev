fad43bcf35a9bd000b8a476e218d000725c0610d
Md. Farhan Zaman <farhanzamanarnob@gmail.com>
2024-05-07 00:04:38 +0600

    GGF

--- version_1.06/backend/api/test.php
+++ version_1.06/backend/api/test.php
@@ -22,7 +22,7 @@ while ($row = $visitorCountByCountryResult->fetch_assoc()) {
 }
 
 // Query to get visitor data for all countries
-$visitorDataByCountryQuery = "SELECT * FROM visitors WHERE is_tracked = 1";
+$visitorDataByCountryQuery = "SELECT * FROM visitors WHERE is_tracked = 1 ORDER BY city_name";
 $visitorDataByCountryResult = $conn->query($visitorDataByCountryQuery);
 $visitorDataByCountry = array();
 while ($row = $visitorDataByCountryResult->fetch_assoc()) {
@@ -47,12 +47,14 @@ $conn->close();
 </head>
 
 <body>
+    
     <div class="container">
         <div class="row">
-            <div class="col">
-                <canvas id="visitorChart"></canvas>
+            <div class="col-1">
+
             </div>
-            <div class="col">
+            <div class="col-2">
+                <canvas class="pieChart" id="visitorChart"></canvas>
                 <div id="visitorDataPopup" class="modal">
                     <div class="modal-content">
                         <div class="modal-header">
@@ -61,21 +63,30 @@ $conn->close();
                         </div>
                         <div class="country-details" id="visitorDataDetails"></div>
                         <div class="modal-body">
-                            <table class="visitor-table">
-                                <thead>
-                                    <tr>
-                                        <th>IP Address</th>
-                                        <th>Latitude</th>
-                                        <th>Longitude</th>
-                                        <th>Subdivision Name</th>
-                                        <th>State Name</th>
-                                        <th>City Name</th>
-                                        <th>First Visit Date</th>
-                                        <th>Last Active Date</th>
-                                    </tr>
-                                </thead>
-                                <tbody id="visitorDataBody"></tbody>
-                            </table>
+                            <div class="tabs">
+                                <button class="tablinks" onclick="openTab(event, 'barChartTab')">Bar Chart</button>
+                                <button class="tablinks" onclick="openTab(event, 'tableTab')">Raw Data</button>
+                            </div>
+                            <div id="barChartTab" class="tab active">
+                                <canvas class="barChart" id="cityChart"></canvas>
+                            </div>
+                            <div id="tableTab" class="tab">
+                                <table class="visitor-table">
+                                    <thead>
+                                        <tr>
+                                            <th>IP Address</th>
+                                            <th>Latitude</th>
+                                            <th>Longitude</th>
+                                            <th>Subdivision Name</th>
+                                            <th>State Name</th>
+                                            <th>City Name</th>
+                                            <th>First Visit Date</th>
+                                            <th>Last Active Date</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody id="visitorDataBody"></tbody>
+                                </table>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -124,6 +135,20 @@ $conn->close();
 
             return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
         }
+        // Function to open tab
+        function openTab(event, tabName) {
+            var i, tabcontent, tablinks;
+            tabcontent = document.getElementsByClassName("tab");
+            for (i = 0; i < tabcontent.length; i++) {
+                tabcontent[i].style.display = "none";
+            }
+            tablinks = document.getElementsByClassName("tablinks");
+            for (i = 0; i < tablinks.length; i++) {
+                tablinks[i].classList.remove("active");
+            }
+            document.getElementById(tabName).style.display = "block";
+            event.currentTarget.classList.add("active");
+        }
         // Visitor count by country pie chart
         var visitorData = <?php echo json_encode($visitorCountByCountryData); ?>;
         var visitorLabels = Object.keys(visitorData);
@@ -143,15 +168,17 @@ $conn->close();
             options: {
                 responsive: true,
                 maintainAspectRatio: false,
-                onClick: function(event, elements) {
+                onClick: function (event, elements) {
                     if (elements && elements.length > 0) {
                         var index = elements[0].index;
                         var label = visitorChart.data.labels[index];
                         var visitorData = <?php echo json_encode($visitorDataByCountry); ?>;
                         var countryVisitorData = visitorData[label];
                         var visitorDataHtml = '';
+                        var cityVisitorData = {};
                         if (countryVisitorData) {
-                            countryVisitorData.forEach(function(visitor) {
+                            countryVisitorData.forEach(function (visitor) {
+                                cityVisitorData[visitor.city_name] = (cityVisitorData[visitor.city_name] != null ? cityVisitorData[visitor.city_name] : 0) + 1;
                                 visitorDataHtml += '<tr>';
                                 visitorDataHtml += '<td>' + visitor.visitor_ip + '</td>';
                                 visitorDataHtml += '<td>' + visitor.location_latitude + '</td>';
@@ -164,7 +191,49 @@ $conn->close();
                                 // Add more table cells as needed
                                 visitorDataHtml += '</tr>';
                             });
+                            console.log(cityVisitorData);
+                            var sortedCityData = Object.entries(cityVisitorData).sort((a, b) => b[1] - a[1]);
 
+                            // Extract sorted labels and values
+                            var sortedCityLabels = sortedCityData.map(entry => entry[0]);
+                            var sortedCityValues = sortedCityData.map(entry => entry[1]);
+
+                            // Create the bar chart with sorted data
+                            var cityChartCanvas = document.getElementById('cityChart').getContext('2d');
+                            var existingChart = Chart.getChart(cityChartCanvas);
+
+                            // If an existing chart exists, destroy it
+                            if (existingChart) {
+                                existingChart.destroy();
+                            }
+                            var cityChart = new Chart(cityChartCanvas, {
+                                type: 'bar',
+                                data: {
+                                    labels: sortedCityLabels,
+                                    datasets: [{
+                                        label: 'Visitor Count',
+                                        data: sortedCityValues,
+                                        backgroundColor: generateUniqueColors(sortedCityLabels.length),
+                                        borderColor: 'rgba(54, 162, 235, 1)',
+                                        borderWidth: 1
+                                    }]
+                                },
+                                options: {
+                                    indexAxis: 'y',
+                                    scales: {
+                                        x: {
+                                            ticks: {
+                                                autoSkip: false, // Prevents labels from being skipped
+                                                maxRotation: 45, // Rotates labels to be vertical
+                                                minRotation: 45 // Rotates labels to be vertical
+                                            }
+                                        },
+                                        y: {
+                                            beginAtZero: true
+                                        }
+                                    }
+                                }
+                            });
                             document.getElementById('visitorDataTitle').innerHTML = 'Visitors - ' + countryVisitorData[0].country_name + ' (' + countryVisitorData[0].country_iso_code + ')';
                             document.getElementById('visitorDataDetails').innerHTML = '<b>Continent:</b> ' + countryVisitorData[0].continent + ' | <b>Phone Code:</b> ' + countryVisitorData[0].country_phone_code + ' | <b>Currency:</b> ' + countryVisitorData[0].country_currency;
                             document.getElementById('visitorDataBody').innerHTML = visitorDataHtml;
